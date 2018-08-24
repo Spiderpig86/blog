e8f7f0abf4956696a46623141c02e4f8aedeeade
Spiderpig86 <slim679975@gmail.com>
2018-08-24 14:55:31 -0400

    chore(gatsby-config.js): Setting up for deploy

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,18 +1,19 @@
 module.exports = {
     siteMetadata: {
-        title: `The Blog`,
+        title: `Slim Blog`,
     },
     plugins: [
         `gatsby-plugin-react-helmet`, // To modify head tags
         `gatsby-plugin-catch-links`, // Intercepts links from markdown to avoid refreshes
         `gatsby-plugin-sass`,
         {
-        resolve: `gatsby-source-filesystem`,
-        options: {
-            path: `${__dirname}/src/pages`, // Store markdown posts here,
-            name: 'pages'
-        }
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                path: `${__dirname}/src/pages`, // Store markdown posts here,
+                name: 'pages'
+            }
         },
         `gatsby-transformer-remark` // Markdown to HTML
     ],
+    pathPrefix: "/blog", // For setting up in github pages
 }
