df6a0526999f4194e7b3ee489d29b7283ed817e6
Stanley Lim <slim679975@gmail.com>
2020-07-26 15:21:04 -0700

    :wrench: chore(config): Updated blog url

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    url: 'https://spiderpig86.github.io/',
+    url: 'https://slim.netlify.app/',
     title: 'slim blog',
     description: 'A personal blog of mine talking about tech, life, and whatever I found intersting.',
     keyword: 'slim, stanley lim, blog, gatsby',
-    pathPrefix: `/`
+    pathPrefix: ``
 }
