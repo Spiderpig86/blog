249ca75f08119979cbb67142b2b4ac345dacf18e
Stanley Lim <slim679975@gmail.com>
2021-01-03 20:58:40 -0500

    :sparkles: feat(gatsby-config.js): Added Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -70,6 +70,17 @@ module.exports = {
       },
     },
     `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: '257518965',
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        // Defers execution of google analytics script after page load
+        defer: false,
+      },
+    },
   ],
   // pathPrefix: `/blog`, // For setting up in github pages
 }
