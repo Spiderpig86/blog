f9f3a8b927a6753bf5925a072959a5d968fc27ab
Stanley Lim <slim679975@gmail.com>
2021-01-19 01:04:53 -0500

    :sparkles: feat(sidebar.js): Added newsletter signup form

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -2,9 +2,9 @@ import React from 'react'
 import Link from 'gatsby-link'
 import ColorHash from 'color-hash'
 
-import { styles } from '../styles/component-styles/sidebar-styles';
+import { styles } from '../styles/component-styles/sidebar-styles'
 
-const Sidebar = props => (
+const Sidebar = (props) => (
   <div
     style={{
       backgroundColor: 'var(--bg-secondary)',
@@ -14,36 +14,96 @@ const Sidebar = props => (
       marginBottom: '2rem',
       marginTop: '1rem',
       top: '7rem',
-      position: props.sticky ? 'sticky' : 'inherit'
+      position: props.sticky ? 'sticky' : 'inherit',
     }}
   >
     <div className="row">
       ✏️ Published <strong>{props.date}</strong>
     </div>
-    <div className="row" style={{
-      marginBottom: '1rem'
-    }}>
-      ⏲ <strong>{props.duration}</strong> { props.duration === 1 ? 'minute' : 'minutes' }
+    <div
+      className="row"
+      style={{
+        marginBottom: '1rem',
+      }}
+    >
+      ⏲ <strong>{props.duration}</strong>{' '}
+      {props.duration === 1 ? 'minute' : 'minutes'}
     </div>
 
-    <div className="row" style={{
-      marginBottom: '1rem'
-    }}>
-        {props.tags.map((tag, i) => {
-            const colorHash = new ColorHash({lightness: 0.5})
-            const color = colorHash.hex(tag);
+    <div
+      className="row"
+      style={{
+        marginBottom: '1rem',
+      }}
+    >
+      {props.tags.map((tag, i) => {
+        const colorHash = new ColorHash({ lightness: 0.5 })
+        const color = colorHash.hex(tag)
 
-            return (
-              <Link to={ `/tag/${tag}` } style={{
-                ...styles.tagStyle,
-                backgroundColor: color,
-                color: '#fff'
-              }} key={i}>
-                {tag}
-              </Link>
-            )
-        })}
+        return (
+          <Link
+            to={`/tag/${tag}`}
+            style={{
+              ...styles.tagStyle,
+              backgroundColor: color,
+              color: '#fff',
+            }}
+            key={i}
+          >
+            {tag}
+          </Link>
+        )
+      })}
     </div>
+
+    <hr
+      style={{
+        backgroundColor: 'var(--hr)',
+        marginBottom: '2rem',
+      }}
+    />
+    <p>
+      <b>Subscribe</b> to receive notifications for any <b>new post</b>!
+    </p>
+    <form action="https://stanleylim.us2.list-manage.com/subscribe/post">
+      <div className="form-group">
+        <input
+          type="email"
+          className="form-group-input input-small"
+          autoCapitalize="off"
+          autoCorrect="off"
+          name="MERGE0"
+          id="MERGE0"
+          placeholder="Email"
+          defaultValue=""
+          style={{
+            color: '#222',
+            lineHeight: 'normal',
+          }}
+        />
+        <input
+          type="hidden"
+          name="u"
+          defaultValue="c29a21f0dd7ce0561ec3adb9f"
+          
+        />
+        <input type="hidden" name="id" defaultValue="f783a43c04" />{' '}
+        <input
+          type="hidden"
+          name="ht"
+          defaultValue="ed00e8c36ca669627ce34a5802d7c3f956e35fa7:MTYwMzYxNzkyNy40MzE0"
+        />
+        <button type="submit" className="form-group-btn btn-small btn-primary uppercase">
+          Subscribe
+        </button>
+      </div>
+    </form>
+    <span>
+      Or follow this RSS feed
+      <a className="ml-1" href="/rss.xml" target="_blank">
+        <i class="fa fa-rss"></i>
+      </a>
+    </span>
   </div>
 )
 
